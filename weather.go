e15691773363f1a5dab991ddba6632b025dff638
jziegener <jziegener@gmail.com>
2022-01-08 19:03:54 -0500

    read json response into struct

--- weather.go
+++ weather.go
@@ -1,18 +1,59 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"net/url"
 	"net/http"
+	"net/url"
 	"os"
-	"io"
 )
 
 var (
 	apiKeyName string = "WEATHER_APIKEY"
-	baseURL string = "http://api.weatherapi.com/v1/current.json"
+	baseURL    string = "http://api.weatherapi.com/v1/current.json"
 )
 
+type WeatherResp struct {
+	Location struct {
+		Name           string  `json:"name"`
+		Region         string  `json:"region"`
+		Country        string  `json:"country"`
+		Lat            float64 `json:"lat"`
+		Lon            float64 `json:"lon"`
+		TzID           string  `json:"tz_id"`
+		LocaltimeEpoch int     `json:"localtime_epoch"`
+		Localtime      string  `json:"localtime"`
+	} `json:"location"`
+	Current struct {
+		LastUpdatedEpoch int     `json:"last_updated_epoch"`
+		LastUpdated      string  `json:"last_updated"`
+		TempC            float64 `json:"temp_c"`
+		TempF            float64 `json:"temp_f"`
+		IsDay            int     `json:"is_day"`
+		Condition        struct {
+			Text string `json:"text"`
+			Icon string `json:"icon"`
+			Code int    `json:"code"`
+		} `json:"condition"`
+		WindMph    float64 `json:"wind_mph"`
+		WindKph    float64 `json:"wind_kph"`
+		WindDegree int     `json:"wind_degree"`
+		WindDir    string  `json:"wind_dir"`
+		PressureMb float64 `json:"pressure_mb"`
+		PressureIn float64 `json:"pressure_in"`
+		PrecipMm   float64 `json:"precip_mm"`
+		PrecipIn   float64 `json:"precip_in"`
+		Humidity   int     `json:"humidity"`
+		Cloud      int     `json:"cloud"`
+		FeelslikeC float64 `json:"feelslike_c"`
+		FeelslikeF float64 `json:"feelslike_f"`
+		VisKm      float64 `json:"vis_km"`
+		VisMiles   float64 `json:"vis_miles"`
+		Uv         float64 `json:"uv"`
+		GustMph    float64 `json:"gust_mph"`
+		GustKph    float64 `json:"gust_kph"`
+	} `json:"current"`
+}
 
 func getWeather(apikey string) string {
 	baseUrl, err := url.Parse(baseURL)
@@ -24,23 +65,25 @@ func getWeather(apikey string) string {
 	query := url.Values{}
 	//query.Add("domain", baseURL)
 	query.Add("key", apikey)
-	query.Add("q", "London")
-
+	query.Add("q", "auto:ip")
 	baseUrl.RawQuery = query.Encode()
 
-
-	fmt.Println(baseUrl.String())
-
+	fmt.Println("Making Request: ", baseUrl.String())
 
 	resp, err := http.Get(baseUrl.String())
+	if err != nil {
+		fmt.Println("Error with request:: ", err.Error())
+		return ""
+	}
+	defer resp.Body.Close()
+	var w WeatherResp
+	decoder := json.NewDecoder(resp.Body)
+	err = decoder.Decode(&w)
 
-	//if err != nil {
-	//	fmt.Println("Error with request:: ", err.Error())
-	//	return ""
-	//}
-	//fmt.Println(resp)
-	b, err := io.ReadAll(resp.Body)
-	fmt.Println(string(b))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(w)
 	return ""
 }
 
@@ -60,4 +103,3 @@ func main() {
 	fmt.Println(getWeather(apiKey))
 
 }
-
