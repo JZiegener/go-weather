19ce101d5a86e1cc347d6c381daa43a04a361790
jziegener <jziegener@gmail.com>
2022-01-11 20:57:32 -0500

    Add conditional coloring for temp. Only works for metric

--- formatOutput.go
+++ formatOutput.go
@@ -5,7 +5,30 @@ import (
 	"github.com/fatih/color"
 	"os"
 	"strconv"
+	"math"
 )
+
+func clamp(val, min, max float64) float64 {
+	return math.Min(math.Max(val,min), max)
+
+}
+
+func colorlerp(val, min, max float64) color.Attribute {
+	var heatColorArray[5]color.Attribute
+	heatColorArray[0] =	color.FgCyan
+	heatColorArray[1] =	color.FgBlue
+	heatColorArray[3] =	color.FgGreen
+	heatColorArray[3] =	color.FgYellow
+	heatColorArray[4] =	color.FgRed
+	percent := clamp(val-min, 0, max)/max
+	index := math.Round(percent*4)
+	return heatColorArray[int(index)]
+}
+
+func colorTemp(v float64) string {
+	color := color.New(colorlerp(v, -10,32))
+	return color.SprintFunc()(strconv.FormatFloat(v, 'f', -1, 64))
+}
 
 func colorf(v float64, c *color.Color) string {
 	return c.SprintFunc()(strconv.FormatFloat(v, 'f', -1, 64))
@@ -28,8 +51,8 @@ func printWeather(w weatherResp, useMetric bool) {
 	fmt.Fprintf(os.Stdout, "Weather report: %s, %s, %s\n", w.Location.Name, w.Location.Region, w.Location.Country)
 	fmt.Fprintf(os.Stdout, "\t%s\n", report.Condition)
 	fmt.Fprintf(os.Stdout, "\t%s (%s) %s\n",
-		colorf(report.Temp, cyan),
-		colorf(report.FeelsLike, cyan), units.temp)
+		colorTemp(report.Temp),
+		colorTemp(report.FeelsLike), units.temp)
 	fmt.Fprintf(os.Stdout, "\t%s - %s %s\n", colorf(report.Wind, cyan), colorf(report.WindGust, cyan), units.speed)
 	fmt.Fprintf(os.Stdout, "\t%s %s\n", colorf(report.Visiblity, cyan), units.distance)
 	fmt.Fprintf(os.Stdout, "\t%s %s", colorf(report.Percip, cyan), units.volume)
